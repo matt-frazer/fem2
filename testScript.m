e9bb0d54bab23dd4c08902cef2853182193ec53b
Matt Coppola <macoppol@uwaterloo.ca>
2014-10-14 15:38:29 -0400

    Cleaned up Test Script, and seperated plotting

--- testScript.m
+++ testScript.m
@@ -22,34 +22,44 @@ for a = 1:2:20002
 end
 
 % timeSteps and StopTime populated based on question perhaps? 
-e = 0.1; %delta t 
-e2 = 1;
-e3 = 0.01;
+timeStep = 1; %delta t 
+timeStep2 = 0.1; 
+timeStep3 = 0.01;
 timeStop = 5; %end of for loop
 
 
 % the three calls to explicit calc at each time increment
-% u3 = explicitCalc(e3, timeStop, u, force, m, k, c, initConditions);
-% u2 = explicitCalc(e2, timeStop, u, force, m, k, c, initConditions);
-% u = explicitCalc(e, timeStop, u, force, m, k, c, initConditions);
+uExplicit = explicitCalc(timeStep, timeStop, u, force, m, k, c, initConditions);
+uExplicit2 = explicitCalc(timeStep2, timeStop, u, force, m, k, c, initConditions);
+uExplicit3 = explicitCalc(timeStep3, timeStop, u, force, m, k, c, initConditions);
  
 % Implicit calc at each time step
-u3 = implicitCalc(e3, timeStop, u, force, m, k, c, gamma, beta, initialConditions);
-u2 = implicitCalc(e2, timeStop, u, force, m, k, c, gamma, beta, initialConditions);
-u = implicitCalc(e, timeStop, u, force, m, k, c, gamma, beta, initialConditions);
+uImplicit = implicitCalc(timeStep, timeStop, u, force, m, k, c, gamma, beta, initialConditions);
+uImplicit2 = implicitCalc(timeStep2, timeStop, u, force, m, k, c, gamma, beta, initialConditions);
+uImplicit3 = implicitCalc(timeStep3, timeStop, u, force, m, k, c, gamma, beta, initialConditions);
 
-%axis for ploting
-t = 0:e:timeStop;
-t2 = 0:e2:timeStop;
-t3 = 0:e3:timeStop;
+%Time Axis for ploting
+t = 0:timeStep:timeStop;
+t2 = 0:timeStep2:timeStop;
+t3 = 0:timeStep3:timeStop;
 
-%geting the X displacements for plotting
-ux = u(1:2:102);
-ux2 = u2(1:2:12);
-ux3 = u3(1:2:1002);
+%Plotting Explicit
+expDisplacement = uExplicit(1:2:12);
+expDisplacement2 = uExplicit2(1:2:102);
+expDisplacement3 = uExplicit3(1:2:1002);
 
-%stacked ploting
-hold all;
-plot(t, ux, 'red');
-plot(t2, ux2, 'blue');
-plot(t3, ux3, 'green');
+hold all; %stacked ploting
+plot(t, expDisplacement, 'red');
+plot(t2, expDisplacement2, 'blue');
+plot(t3, expDisplacement3, 'green');
+
+%Plotting Implicit
+impDisplacement = uImplicit(1:2:12);
+impDisplacement2 = uImplicit2(1:2:102);
+impDisplacement3 = uImplicit3(1:2:1002);
+
+figure(2);
+hold all; %stacked ploting
+plot(t, impDisplacement, 'red');
+plot(t2, impDisplacement2, 'blue');
+plot(t3, impDisplacement3, 'green');
